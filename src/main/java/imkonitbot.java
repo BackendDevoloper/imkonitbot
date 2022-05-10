e3e56e7bb30bd471db34efdb725e347fd3247fb9
BackendDevoloper <105289530+BackendDevoloper@users.noreply.github.com>
2022-05-10 17:52:40 +0500

    add libraries

--- src/main/java/imkonitbot.java
+++ src/main/java/imkonitbot.java
@@ -1,2 +1,21 @@
-public class imkonitbot {
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.objects.Update;
+
+public class imkonitbot extends TelegramLongPollingBot {
+
+
+
+    @Override
+    public void onUpdateReceived(Update update) {
+        System.out.println("salom hammaga");
+    }
+
+    @Override
+    public String getBotUsername() {
+        return "imkonit_bot";
+    }
+    @Override
+    public String getBotToken() {
+        return "5236876239:AAFPm12-tM5lIk-jJd8ZNufc-PW-ciK1-Do";
+    }
 }
